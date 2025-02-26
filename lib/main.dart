9f3c0f81adf2ad601b9fe257d583b85bc9b53a9b
ROBERTO_ALEXIS_GAYTAN_MENDOZA <a.22308051281202@cbtis128.edu.mx>
2025-02-26 20:12:25 +0000

    IMAGEN

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Roberto Alexis Gaytan"),
+          titleTextStyle:
+              const TextStyle(color: Color(0xff284641), fontSize: 20),
+          centerTitle: true,
+          backgroundColor: const Color(0xff82e0d0),
+        ),
+        body: Column(
+          children: [
+            Text(
+              "Roberto Alexis Gaytan Mendoza Mat:22308051281202",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(
+                    Icons.thumb_down_alt_sharp,
+                    color: Colors.pink,
+                    size: 30.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.audiotrack,
+                    color: Color(0xff2846ed),
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.beach_access,
+                    color: Colors.blue,
+                    size: 36.0,
+                  ),
+                  Icon(
+                    Icons.map_sharp,
+                    color: Color(0xff000000),
+                    size: 36.0,
+                  ),
+                ]),
+          ],
         ),
       ),
     );
-  }
-}
+  } // Fin Widgets
+} //Fin Clase MisIconosApp
